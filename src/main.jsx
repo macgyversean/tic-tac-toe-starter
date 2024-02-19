cc0a3cd770c69673bc97016bd53dc33c6272d7c6
Sean Carroll <macgyversean@gmail.com>
2024-02-19 16:03:18 -0500

    added hotfix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Game from './App.jsx'
+import Square from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
